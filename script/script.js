19387b542a0d5709a139cc8dd54cb47cc969e744
Alexandr Biryukov <biralan.mj@gmail.com>
2020-10-19 23:06:38 +0500

    fix: main code |script.js

--- script/script.js
+++ script/script.js
@@ -1,33 +1,29 @@
 'use strict';
-const isNumber = function (n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+const isNumber = function (number) {
+  return !isNaN(parseFloat(number)) && isFinite(number);
 };
-const getUserInputAsNumber = function (question) {
-  let n;
-  do {
-    if (n === null) {
-      return null;
+const game = function () {
+  const randomNumber = Math.floor(Math.random() * 99);
+  return function repeat() {
+    let answer = prompt('Угадай число от 1 до 100');
+    if (isNumber(answer)) {
+      if (answer > randomNumber) {
+        alert('Загаданое число меньше');
+        return repeat();
+      } else if (answer < randomNumber) {
+        alert('Загаданое число больше');
+        return repeat();
+      } else {
+        alert('Поздравляю, Вы угадали!!!');
+      }
+    } else if (!isNumber(answer)) {
+      answer = confirm('Введите число!');
+      if (answer === false) {
+        alert('Игра окончена');
+      } else {
+        return repeat();
+      }
     }
-    if (n !== undefined) {
-      alert('Введи число!');
-    }
-    n = prompt(question);
-  } while (!isNumber(n));
-  return +n;
+  };
 };
-
-const randomNumber = Math.floor(Math.random() * 99) + 1;
-
-function randomGame(question) {
-  let answer = getUserInputAsNumber(question);
-  if (answer === null) {
-    alert('Игра окончена');
-  } else if (answer > randomNumber) {
-    randomGame('Загаданое число меньше');
-  } else if (answer < randomNumber) {
-    randomGame('Загаданое число больше');
-  } else {
-    alert('Поздравляю, Вы угадали!!!');
-  }
-}
-randomGame('Угадай число от 1 до 100');
+game()();
